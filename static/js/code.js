00b0d2d29b96c90d06c9b6bc5e58d46e32e1ef44
Sirish <b17sirish@gmail.com>
2022-05-31 10:03:29 +0530

    added scale and slider

--- static/js/code.js
+++ static/js/code.js
@@ -1,12 +1,13 @@
 pred_btn = document.querySelector('#pred_btn')
 pred_video_btn = document.querySelector('#pred_video_btn')
 stop_btn = document.querySelector('#stop_btn')
-var stop = 0
+var stop_exe = 0
 
 
 model = document.getElementById('models') // current selected model
 
 pred_txt = document.getElementById('pred_txt') // label that show the predicted count
+pred_label = document.getElementById('pred_label') // label that show the predicted count
 pred_img = document.getElementById('pred_img') // predicted density map
 
 // ip address
@@ -58,30 +59,33 @@ const predict = () => {
         .catch(err => console.log(err))
 }
 
-function get_response(url,options,data){
+function get_response(url, options, data) {
 
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST",url,false);
-    xhr.setRequestHeader("Accept","application/json");
-    xhr.setRequestHeader("Content-type","application/json");
+    xhr.open("POST", url, false);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(data))
-    if(xhr.status === 200){
-    let result = JSON.parse(xhr.responseText)
-            let { pred_cnt, pred_time, device ,next_frame} = result
-            pred_cnt = parseFloat(pred_cnt).toFixed(4)
-            pred_time = parseFloat(pred_time).toFixed(4)
-            console.log(next_frame)
-         console.log(result)
+    if (xhr.status === 200) {
+        let result = JSON.parse(xhr.responseText)
+        let { pred_cnt, pred_time, device, next_frame } = result
+        pred_cnt = parseFloat(pred_cnt).toFixed(4)
+        pred_time = parseFloat(pred_time).toFixed(4)
+        // console.log(next_frame)
+        // console.log(result)
 
-            pred_txt.innerHTML = `${pred_cnt}, took ${pred_time} seconds on ${device}`
-            // pred_img.src = [url_address, 'static/map.jpg?'].join('/') + new Date().getTime()
-            /*window.location.href = "http://localhost:5000/predict/" + res;*/
-            // console.log(nextFrame)
-    // result.new_request = false;
-    return result
+        pred_txt.innerHTML = `${pred_cnt}, took ${pred_time} seconds on ${device}`
+        if(parseInt(pred_cnt) > parseInt(output.innerHTML)) {pred_txt.style.color = "red"; pred_label.style.color = "red";}
+        else {pred_txt.style.color = "green"; pred_label.style.color = "green";}
+        // pred_img.src = [url_address, 'static/map.jpg?'].join('/') + new Date().getTime()
+        /*window.location.href = "http://localhost:5000/predict/" + res;*/
+        // console.log(nextFrame)
+        // result.new_request = false;
+        return result
 
-}}
+    }
+}
 
 const predict_video = async () => {
     if (
@@ -93,10 +97,10 @@ const predict_video = async () => {
 
 
     url = [url_address, 'predict_video'].join('/')
-    let result = {next_frame: true, new_request: true}
+    let result = { next_frame: true, new_request: true }
     let data;
     let options;
-    while (result.next_frame && stop===0) {
+    while (result.next_frame && stop_exe === 0) {
         data = {
             model: model.value,
             video: true,
@@ -147,12 +151,22 @@ window.addEventListener("load", () => {
 })
 
 pred_video_btn.onclick = e => {
-    stop = 0
+    e.preventDefault()
+    stop_exe = 0
     predict_video()
 }
 pred_btn.onclick = e => {
     predict()
 }
 stop_btn.onclick = e => {
-    stop = 1;
+    console.log(stop_exe)
+    stop_exe = 1;
 }
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
